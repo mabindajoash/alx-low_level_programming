9a751412e9b22d1a9b755f3c54d2224409843bcf
mabindajoash <mabindajoash@gmail.com>
2024-06-20 13:30:33 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - takes no arguments
+ * Return: 0 value always
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
        	return 0;
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - takes no arguments
+ * Return: 0 value always
+ */
+int main(void)
+{
+	char (C);
+	int (I);
+	long int(L);
+	long long int (LL);
+	float (F);
+
+	printf("Size of char: %d bytes(s)\n", sizeof(C));
+	printf("Size of int: %i byte(s)\n", sizeof(I));
+	printf("Size of long int: %li byte(s)\n", sizeof(L));
+	printf("Size of long long int: %d byte(s)\n", sizeof(LL));
+	printf("Size of float: %d byte(s)\n", sizeof(F));
+
+	return (0);
+}
