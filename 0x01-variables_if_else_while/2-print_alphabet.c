786d925bbe7b039b34ffd6766c0677a5b05f5040
mabindajoash <mabindajoash@gmail.com>
2024-06-24 03:15:03 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdipo.h>
+
+/**
+  * main - entry point takes no argument
+  *Return: Value is 0
+  */
+int main(void)
+{
+	char (n);
+
+	for (i = 'a'; i < = 'z'; i++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
