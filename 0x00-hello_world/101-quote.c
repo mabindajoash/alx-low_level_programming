2be5f894821f8db1b015e6299da1e14bf739b7a3
mabindajoash <mabindajoash@gmail.com>
2024-06-24 02:21:07 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *message;
 
-	message = "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, strlen(message));
 	return (1);
 }
