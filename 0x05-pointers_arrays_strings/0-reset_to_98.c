78fae9c9feeb4c50243486165ab5f205d37f8fd2
mabindajoash <mabindajoash@gmail.com>
2024-07-01 10:26:32 -0700

    Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,9 @@
 #include "main.h"
 
 /**
-  *reset_to_98 - take a pointer and update the value of the pointer to 98
-  *Return: nothing is returned
+  * reset_to_98 - take a pointer and update the value of the pointer to 98
+  * @n: the pointer
+  * Return: nothing is returned
   */
 void reset_to_98(int *n)
 {
