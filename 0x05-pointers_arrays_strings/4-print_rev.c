14423efdc323115c132b0be04b6b1ae604b6bf23
mabindajoash <mabindajoash@gmail.com>
2024-07-01 13:56:00 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *print_rev - print string in reverse
+  *@s: takes char s as a parameter
+  *Return: void
+  */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	while (s[i++])
+		length++;
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
