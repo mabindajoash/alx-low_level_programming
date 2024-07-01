87f124c17318eb0e22993f751e56ca9067ee2299
mabindajoash <mabindajoash@gmail.com>
2024-07-01 14:21:55 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,9 @@
 void print_rev(char *s)
 {
 	int length = 0;
-	int i;
-
-	while (s[i] != '\n')
+	while (s[length] != '\0')
 		length++;
-	for (i = length - 1; i >= 0; i--)
-	{
+	for (int i = length - 1;length >= 0; i--)
 		_putchar(s[i]);
-	}
-
 	_putchar('\n');
 }
