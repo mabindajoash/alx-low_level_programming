56ac73c083d531b425a095026e20ff35eb09632c
mabindajoash <mabindajoash@gmail.com>
2024-07-09 07:52:55 -0700

    spin

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _strspn - returns the number of bytes that match
+  * @s: take in char *s as argument
+  * @accept: takes in accept as an argument
+  * Return: number of bytes
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int match;
+
+	while (*s)
+	{
+		match = 0;
+		char *a;
+		
+		for (a = accept; *a; ++a)
+		{
+			if (*s == *a)
+			{
+				match = 1;
+				break;
+			}
+		}
+		if (!match)
+			break;
+		++count;
+		++s;
+	}
+	return (count);
+}
