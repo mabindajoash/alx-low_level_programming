7fd2bd38ec0b34a2ec1ec480b40adaabcb8d7858
mabindajoash <mabindajoash@gmail.com>
2024-07-10 10:35:23 -0700

    final

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,12 +8,10 @@
   */
 int factorial(int n)
 {
-	int count = 0;
-
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	else if (n == 0 || n == 1)
 		return (1);
 	else
-		return (count + factorial(n - 1));
+		return (n * factorial(n - 1));
 }
