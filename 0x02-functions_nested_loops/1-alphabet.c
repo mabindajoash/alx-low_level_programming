5b04424a3a9cd65202c3528c9579882f66157732
mabindajoash <mabindajoash@gmail.com>
2024-07-11 03:08:53 -0700

    lowcase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_alphabet - prints alphabet in lowercase followed by a new line
+  * Return: void
+  */
+
+void print_alphabet(void)
+{
+	for (int i = 'a'; i < 'z'; i++)
+	{
+		putchar('i');
+	}
+	putchar('\n');
+	return;
+}
