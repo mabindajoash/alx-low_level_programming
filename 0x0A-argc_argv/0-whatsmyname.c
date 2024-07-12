b2d743d7ec21b46b3002f3f9ebdc82f7d142f507
mabindajoash <mabindajoash@gmail.com>
2024-07-12 05:39:24 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
 /**
   * main - a programme that prints its name
   * @argc: count the number of arguments
-  * @argv[]: array of strings
+  * @argv: array of strings
   * Return: 0
   */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	printf("%s \n", argv[0]);
+	if (argc > 0)
+	{
+		printf("%s \n", argv[0]);
+	}
 	return (0);
 }
