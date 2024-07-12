7c2d97d86ddecfb2cfd3ca509bf58861d21d5ebc
mabindajoash <mabindajoash@gmail.com>
2024-07-12 06:10:21 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
   * main - prints the number of arguments passed to it
   * @argc: number of arguments
-  * Return: 0
+  * argv: array of arguments
+  * Return: 0 value
   */
 int main(int argc, char *argv[])
 {
 	argv = argv;
-	printf("%d \n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
