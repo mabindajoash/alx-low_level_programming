1631e84a1c35462cb41d8c652d08ebec0ab3c74b
mabindajoash <mabindajoash@gmail.com>
2024-07-12 05:29:29 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - a programme that prints its name
+  * @argc: count the number of arguments
+  * @argv[]: array of strings
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s \n", argv[0]);
+	return (0);
+}
