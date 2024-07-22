3c9bdf7f1834772de3740e7725191bfe50517d53
mabindajoash <mabindajoash@gmail.com>
2024-07-22 04:36:03 -0700

    object-like macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef __0-OBJECT_LIKE_MACRO__H__
-#define __0-OBJECT_LIKE_MACRO__H__
+#ifndef 0-OBJECT_LIKE_MACRO__H__
+#define 0-OBJECT_LIKE_MACRO__H__
 
-#define SIZE 1024;
+#define SIZE 1024
 
 #endif
