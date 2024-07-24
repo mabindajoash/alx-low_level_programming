5b0c0ccb04518c97877cae2827e9b1cd2d1688a8
mabindajoash <mabindajoash@gmail.com>
2024-07-24 13:11:38 -0700

    cmp

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,10 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (cmp == NULL || array == NULL || size <= 0)
 		return (-1);
-	if (cmp == NULL || array == NULL)
-		return ('\0');
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
