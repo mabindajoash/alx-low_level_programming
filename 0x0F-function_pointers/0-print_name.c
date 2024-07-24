d727fc96a28a290e12ef774c3f35977dfa1c2896
mabindajoash <mabindajoash@gmail.com>
2024-07-24 10:59:37 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  * print_name - prints a name
+  * @name: name
+  * 2f: function pointer
+  * Return: void
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef Function_pointers_H
+#define Function_pointers_H
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
