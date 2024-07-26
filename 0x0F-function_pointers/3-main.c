b49400a9d8831cbc43b7af0cda33fa52e6446d34
mabindajoash <mabindajoash@gmail.com>
2024-07-26 08:39:29 -0700

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,9 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	if ((strcmp(argv[2], "+") != 0) && (strcmp(argv[2], "-") != 0) && (strcmp(argv[2], "%") != 0) && (strcmp(argv[2], "/") != 0) && (strcmp(argv[2], "*") != 0))
+	if ((strcmp(argv[2], "+") != 0) && (strcmp(argv[2], "-") != 0) &&
+			(strcmp(argv[2], "%") != 0) && (strcmp(argv[2], "/") != 0) &&
+			(strcmp(argv[2], "*") != 0))
 	{
 		printf("Error\n");
 		exit(99);
@@ -30,7 +32,7 @@ int main(int argc, char *argv[])
 	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && (b == 0))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	operation = get_op_func(argv[2]);
 	printf("%d \n", operation(a, b));
