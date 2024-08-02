80044b475f863a6619cf882af497a839993999ab
mabindajoash <mabindajoash@gmail.com>
2024-08-02 04:02:18 -0700

    count list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,9 +8,9 @@ size_t list_len(const list_t *h)
 {
 	unsigned int counter = 0;
 
-	if (h != NULL)
-		counter++;
-	while (h->next != NULL)
+	if (h == NULL)
+		return(0);
+	while (h != NULL)
 	{
 		counter++;
 		h = h->next;
