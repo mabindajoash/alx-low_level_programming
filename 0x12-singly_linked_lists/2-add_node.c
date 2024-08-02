1975da6f7d238156139fd9cab7e12be9b2dccad0
mabindajoash <mabindajoash@gmail.com>
2024-08-02 04:20:00 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,16 +10,14 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	int size = 0;
 
-	if (*head == NULL)
-		return (NULL);
-	while (str[size++])
+	while (str[size])
 		size++;
 	new_node = (list_t*)malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = strdup(str);
 	if (str == NULL)
 		return (NULL);
+	new_node->str = strdup(str);
 	new_node->len = size;
 	new_node->next = *head;
 	*head = new_node;
