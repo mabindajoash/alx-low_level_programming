f7d08cb9e4dee6dbdcf4efe62ec574d6acde23a7
mabindajoash <mabindajoash@gmail.com>
2024-08-06 04:25:10 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * pop_listint delete the head node
+  * @head: head of the list
+  * Return: head node's data
+  */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+
+	if (*head == NULL)
+		return (0);
+	n = (*head)->n;
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
