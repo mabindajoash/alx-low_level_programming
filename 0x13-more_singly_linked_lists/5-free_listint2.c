7744b4ab50ec9d72bd7937b4cec27872a6e20939
mabindajoash <mabindajoash@gmail.com>
2024-08-06 03:43:22 -0700

    free head make it null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
 		temp = *head;
-		*head = *head->next;
+		*head = (*head)->next;
 		free(temp);
 	}
 	*head = NULL;
