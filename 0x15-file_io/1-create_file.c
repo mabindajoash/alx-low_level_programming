6c15b24b5745f330061789b68f01c17e3853a798
mabindajoash <mabindajoash@gmail.com>
2024-08-13 06:50:55 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		while (lenght)
 			lenght++;
 	}
-	fd = open(filename, O_CREAT | O_TRUNC |O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, lenght);
 	if (w == -1 || fd == -1)
 	{
