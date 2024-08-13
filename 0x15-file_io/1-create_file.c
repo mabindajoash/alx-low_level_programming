a9969a7ae6e68668ffdd41c68109ed8c9f9b93f1
mabindajoash <mabindajoash@gmail.com>
2024-08-13 07:09:44 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, w;
 	int lenght = 0;
 
-	if (filename ==NULL)
+	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
