cf4ce23987ec28a61bc88c8193e802fe089d9418
mabindajoash <mabindajoash@gmail.com>
2024-09-19 09:21:07 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * free_dlistint - free a list
+  * @head: head of the list
+  * Return: 0
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
