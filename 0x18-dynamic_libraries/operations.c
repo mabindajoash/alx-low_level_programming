82ca9c8940aac2fc08ed8ca7efc807a7b9aea53e
mabindajoash <mabindajoash@gmail.com>
2024-09-24 03:03:55 -0700

    101

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+  * add - add two numbers
+  * @a: first no
+  * @b: second no
+  * Return: sum
+  */
+int add(int a, int b)
+{
+	return a + b;
+}
+
+/**
+  * sub - subtract two numbers
+  * @a: first no
+  * @b: second no
+  * Return: difference
+  */
+int sub(int a, int b)
+{
+	return a - b;
+}
+/**
+  * mul - multiply two numbers
+  * @a: first no
+  * @b: second no
+  * Return: product
+  */
+int mul(int a, int b)
+{
+	return a * b;
+}
+/**
+  * mod - remainder of division of two numbers
+  * @a: first no
+  * @b: second no
+  * Return: remainder
+  */
+int mod(int a, int b)
+{
+	return a % b;
+}
+
+/**
+  * div - divide two numbers
+  * @a: first no
+  * @b: second no
+  * Return: division
+  */
+int div(int a, int b)
+{
+	return a / b;
+}
