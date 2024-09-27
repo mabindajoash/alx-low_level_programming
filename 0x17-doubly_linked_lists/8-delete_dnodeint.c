eb3442a9d97287671c0c27e472e11c36a21707eb
mabindajoash <mabindajoash@gmail.com>
2024-09-27 05:51:46 -0700

    delete

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+  * delete_dnodeint_at_index - delete a node at an index
+  * @head: head of the list
+  * @index: index to be deleted
+  * Return: 1 if success -1 on failure
+  */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (0);
+	}
+	temp = *head;
+	while(temp != NULL && index <= count)
+	{
+		if (index == count)
+		{
+			if (temp->prev != NULL)
+				temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			free(temp);
+		}
+		count++;
+		temp = temp->next;
+	}
+	return(-1);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -29,7 +29,7 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int sum_dlistint(dlistint_t *head);
-
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 
 #endif
