b28dc3098090419e87c40ad5b36052d9bef60f3d
mabindajoash <mabindajoash@gmail.com>
2024-10-11 08:50:51 -0700

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+	table->array = (hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
 	return (table);
 }
