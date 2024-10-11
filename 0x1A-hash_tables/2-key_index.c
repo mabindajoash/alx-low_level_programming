e350b58dc98b2a0226923638ea8ff944d8a29310
mabindajoash <mabindajoash@gmail.com>
2024-10-11 09:48:49 -0700

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+  * key_index - returns the index of a key
+  * @key: key
+  * @size: size of the table
+  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
