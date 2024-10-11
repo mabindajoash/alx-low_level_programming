d9c1b7ab1947fa19d66564d23082d386b9e187ab
mabindajoash <mabindajoash@gmail.com>
2024-10-11 08:48:39 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+  * hash_table_create - creates a hash table
+  * @size: size of the hashtable
+  * Return: pointer to a newly created hash table
+  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = (hash_node_t**)calloc(table->size, sizeof(hash_node_t*));
+	return (table);
+}
